6238bc68459814bd6a6eca3173cd344967900e3a
cheyuan1115 <cheyuan@gmail.com>
2025-04-25 17:39:37 +0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StockDashboard from './pages/Dashboard'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StockDashboard />
+  </React.StrictMode>
+)
