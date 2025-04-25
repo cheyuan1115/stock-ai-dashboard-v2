07ea89c4f2b4e614dc9f468ccb4519e1c6e80db8
cheyuan1115 <cheyuan@gmail.com>
2025-04-25 17:52:10 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'  // ← 這一行改正
 import StockDashboard from './pages/Dashboard'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StockDashboard />
   </React.StrictMode>
