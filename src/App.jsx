d9fb2d6ab380ce20f6827278c1717c93337a97dd
cheyuan1115 <cheyuan@gmail.com>
2025-04-30 12:50:13 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import StockPage from "./pages/StockPage";
 
 export default function App() {
   return (
-    <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/stock/:symbol" element={<StockPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
